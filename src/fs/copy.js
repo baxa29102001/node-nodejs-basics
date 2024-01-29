4e7b05743195d98ac2005ee50a26c4724198a0e3
rb2193 <bohodir29102001@gmail.com>
2024-01-29 17:08:17 +0500

    feat:copy folder sub task done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
+import { existsSync } from "fs";
+import { mkdir, copyFile, readdir } from "fs/promises";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
 const copy = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(new URL(import.meta.url).href);
+  const projectFolder = dirname(__filename);
+
+  const folderName = join(projectFolder, "files_copy");
+  if (!existsSync(join(projectFolder, "files")) || existsSync(folderName)) {
+    throw new Error("FS operation failed");
+  }
+  try {
+    await mkdir(folderName);
+
+    const files = await readdir(join(projectFolder, "files"));
+    Promise.all(
+      files.map((item) =>
+        copyFile(
+          join(projectFolder, "files", item),
+          join(projectFolder, "files_copy", item)
+        )
+      )
+    );
+  } catch (error) {
+    console.log("error 14", error);
+  }
 };
 
 await copy();
