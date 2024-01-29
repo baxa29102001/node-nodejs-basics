aeb38d29edcacd45df210f315fe2fd29e83526fe
rb2193 <bohodir29102001@gmail.com>
2024-01-29 17:09:12 +0500

    feat:list filename sub task done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { existsSync } from "fs";
+import { readdir } from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(new URL(import.meta.url).href);
+  const projectFolder = dirname(__filename);
+
+  if (!existsSync(join(projectFolder, "files"))) {
+    throw new Error("FS operation failed");
+  }
+  try {
+    const files = await readdir(join(projectFolder, "files"));
+    console.log(files);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await list();
