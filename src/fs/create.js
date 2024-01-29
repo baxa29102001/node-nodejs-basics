91311af702e76ee388993c945c5344bf50cdb3f7
rb2193 <bohodir29102001@gmail.com>
2024-01-29 14:41:52 +0500

    create js fresh text create done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(new URL(import.meta.url).href);
+  const __dirname = path.dirname(__filename);
+
+  const filePath = path.join(__dirname, "files", "fresh.txt");
+  if (fs.existsSync(filePath)) {
+    throw new Error("FS operation failed: File already exists");
+  }
+
+  const content = "I am fresh and young";
+  fs.writeFileSync(filePath, content);
+
+  console.log("File created successfully:", filePath);
 };
 
 await create();
