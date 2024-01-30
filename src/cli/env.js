fc9321eeef1386f550886bdf4d94de55b1f19fc3
rb2193 <bohodir29102001@gmail.com>
2024-01-30 09:59:34 +0500

    feat:cli done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  let string = "";
+  const envs = process.env;
+  Object.keys(envs).forEach((key) => {
+    if (key.match(/^RSS_/)) {
+      string += `${key}=${envs[key]};`;
+    }
+  });
+
+  console.log(string.slice(0, string.length - 1));
 };
 
 parseEnv();
